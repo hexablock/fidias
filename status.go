fdb87f6e0493235e558cfd5c139a2409c9dbfaae
persephony <persephony>
2017-10-02 19:36:21 -0700

    Updated status

--- status.go
+++ status.go
@@ -1,15 +1,20 @@
 package fidias
 
-import chord "github.com/hexablock/go-chord"
+import (
+	chord "github.com/hexablock/go-chord"
+	"github.com/hexablock/hexatype"
+)
 
 // Status contains status information of a node.
 type Status struct {
-	Ring *chord.Status
+	Hash hexatype.HashAlgorithm
+	DHT  *chord.Status
 }
 
 // Status returns the status of this node
 func (fidias *Fidias) Status() *Status {
 	return &Status{
-		Ring: fidias.ring.Status(),
+		Hash: fidias.conf.Hasher().Algorithm(),
+		DHT:  fidias.ring.Status(),
 	}
 }
